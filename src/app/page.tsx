36530a5cda165bb205811a6fce5f0af16841a336
Eldar Faaiz <iniakunbisnisku@gmail.com>
2024-07-27 04:30:51 +0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import WalletComponents from '@/components/WalletComponents';
 
 export default function Page() {
   return (
+      
     <div className="flex flex-col w-96 md:w-[600px]">
       <section className="flex flex-col w-full mb-6 pb-6 border-b border-sky-800">
         <aside className="flex mb-6">
