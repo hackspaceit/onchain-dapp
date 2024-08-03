3de29988c93c9c0e314cad07fd3f5d5048693461
Eldar Faaiz <iniakunbisnisku@gmail.com>
2024-08-03 21:48:51 +0700

    update

--- src/components/SwapComponents.tsx
+++ src/components/SwapComponents.tsx
@@ -36,6 +36,16 @@ export default function SwapComponents() {
     chainId: 8453,
   };
 
+  const NounsToken: Token = {
+    name: 'NOUNS',
+    address: '0x0a93a7BE7e7e426fC046e204C44d6b03A302b631',
+    symbol: 'NOUNS',
+    decimals: 18,
+    image:
+      'https://d3r81g40ycuhqg.cloudfront.net/wallet/wais/44/2b/442b80bd16af0c0d9b22e03a16753823fe826e5bfd457292b55fa0ba8c1ba213-ZWUzYjJmZGUtMDYxNy00NDcyLTg0NjQtMWI4OGEwYjBiODE2',
+    chainId: 8453,
+  };
+
   const USDCToken: Token = {
     name: 'USDC',
     address: '0x833589fcd6edb6e08f4c7c32d4f71b54bda02913',
