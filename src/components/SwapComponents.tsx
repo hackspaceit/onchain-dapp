b1a8f4ad6ce28647adc89b4c2eb872e13e314fc4
Eldar Faaiz <iniakunbisnisku@gmail.com>
2024-08-03 22:15:35 +0700

    update

--- src/components/SwapComponents.tsx
+++ src/components/SwapComponents.tsx
@@ -56,15 +56,7 @@ export default function SwapComponents() {
     chainId: 8453,
   };
 
-  const USDCToken: Token = {
-    name: 'USDC',
-    address: '0x833589fcd6edb6e08f4c7c32d4f71b54bda02913',
-    symbol: 'USDC',
-    decimals: 6,
-    image:
-      'https://d3r81g40ycuhqg.cloudfront.net/wallet/wais/44/2b/442b80bd16af0c0d9b22e03a16753823fe826e5bfd457292b55fa0ba8c1ba213-ZWUzYjJmZGUtMDYxNy00NDcyLTg0NjQtMWI4OGEwYjBiODE',
-    chainId: 8453,
-  };
+  
 
   const WETHToken: Token = {
     name: 'Wrapped Ether',
